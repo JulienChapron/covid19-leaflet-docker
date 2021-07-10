5f05a5fb41185e48a6be6e22bb9c65c18d545075
Julien Chapron <j.chapron@pm.me>
2021-07-10 20:27:05 +0200

    [test-unit] add navigation.spec.js -> test action and getter theme

--- src/tests/unit/navigation.spec.js
+++ src/tests/unit/navigation.spec.js
@@ -1,24 +1,25 @@
 import navigation from "@/store/modules/navigation.js"
 
 describe("store/modules/navigation.js", () => {
+  const state = {
+    theme: 'light'
+  }
   it("1. mutation theme", async () => {
     const testTheme = 'dark'
-    const state = {
-      theme: 'light'
-    }
     navigation.mutations.theme(state, testTheme )
     expect(state).toStrictEqual({
       theme: 'dark'
     })
   });
   it("2. action theme", async () => {
+    const commit = jest.fn()
     const testTheme = 'dark'
-    const state = {
-      theme: 'light'
-    }
-    navigation.mutations.theme(state, testTheme )
-    expect(state).toStrictEqual({
-      theme: 'dark'
-    })
+    await navigation.actions.updateTheme({ commit }, testTheme)
+    expect(commit).toHaveBeenCalledWith(
+      "theme", 'dark')
   });
+  it("3. getter theme", async () => {
+    const actual = navigation.getters.theme(state)
+    expect(actual).toStrictEqual('dark')
+  })
 });
